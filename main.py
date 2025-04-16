df862b7472eb4d5e73326d88b72d9a8d19846700
Marcus Oshodi <147557402+moshodifimio@users.noreply.github.com>
2025-04-16 16:47:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,4 @@ def number():
 
 # main driver function
 if __name__ == '__main__':
-
-    # run() method of Flask class runs the application 
-    # on the local development server.
-    app.run()
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 8000)), debug=True)
